b9e32e3cbf0f328541430f13156604ea29942171
LI Javier Santiago <li_s.javier@hotmail.com>
2020-07-06 11:42:31 -0500

    docs add

---
+++ docs/precache-manifest.8e8bcbd1eb1d804245f8dbff7fbf3e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f34f1d03490d6f4f3644e82202819a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bff9bcf4d5bfa9c94f95",
+    "url": "/static/css/main.1445f83c.chunk.css"
+  },
+  {
+    "revision": "a0a851eed2f1dab46777",
+    "url": "/static/js/2.1ddd75ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ddd75ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff9bcf4d5bfa9c94f95",
+    "url": "/static/js/main.10b8bd82.chunk.js"
+  },
+  {
+    "revision": "c9bb394677fe04f6f339",
+    "url": "/static/js/runtime-main.3df9bd31.js"
+  }
+]);
